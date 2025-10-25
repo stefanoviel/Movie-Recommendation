ea50f15b57c2f6df3acc87388bb60e17ca783776
stefanoviel <stefi.viel@gmail.com>
2025-10-25 09:55:02 +0200

    tensorboard and validation

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import argparse
 from model import SASRec
 from utils import *
 
+from torch.utils.tensorboard import SummaryWriter
+
 def str2bool(s):
     if s not in {'false', 'true'}:
         raise ValueError('Not a valid boolean string')
@@ -14,28 +16,32 @@ def str2bool(s):
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', required=True)
 parser.add_argument('--train_dir', required=True)
-parser.add_argument('--batch_size', default=128, type=int)
+parser.add_argument('--batch_size', default=128, type=int) # TODO: set to 128 or 256 for real training
 parser.add_argument('--lr', default=0.001, type=float)
 parser.add_argument('--maxlen', default=200, type=int)
 parser.add_argument('--hidden_units', default=50, type=int)
 parser.add_argument('--num_blocks', default=2, type=int)
-parser.add_argument('--num_epochs', default=1000, type=int)
+parser.add_argument('--num_epochs', default=30, type=int)
 parser.add_argument('--num_heads', default=1, type=int)
 parser.add_argument('--dropout_rate', default=0.2, type=float)
 parser.add_argument('--l2_emb', default=0.0, type=float)
-parser.add_argument('--device', default='cuda', type=str)
+parser.add_argument('--device', default='cpu', type=str)
 parser.add_argument('--inference_only', default=False, type=str2bool)
 parser.add_argument('--state_dict_path', default=None, type=str)
 parser.add_argument('--norm_first', action='store_true', default=False)
 
+
 args = parser.parse_args()
 if not os.path.isdir(args.dataset + '_' + args.train_dir):
-    os.makedirs(args.dataset + '_' + args.train_dir)
+    os.makedirs(os.path.join(args.dataset + '_' + args.train_dir))
 with open(os.path.join(args.dataset + '_' + args.train_dir, 'args.txt'), 'w') as f:
     f.write('\n'.join([str(k) + ',' + str(v) for k, v in sorted(vars(args).items(), key=lambda x: x[0])]))
 f.close()
 
-if __name__ == '__main__':
+def main():
+
+    writer = SummaryWriter(log_dir=os.path.join(args.dataset + '_' + args.train_dir, 'tensorboard'))
+
     # global dataset
     dataset = data_partition(args.dataset)
 
@@ -78,11 +84,12 @@ if __name__ == '__main__':
             print(args.state_dict_path)
             print('pdb enabled for your quick check, pls type exit() if you do not need it')
             import pdb; pdb.set_trace()
-            
-    
+
+
     if args.inference_only:
         model.eval()
         t_test = evaluate(model, dataset, args)
+        t_valid = evaluate_valid(model, dataset, args)
         print('test (NDCG@10: %.4f, HR@10: %.4f)' % (t_test[0], t_test[1]))
     
     # ce_criterion = torch.nn.CrossEntropyLoss()
@@ -112,31 +119,37 @@ if __name__ == '__main__':
             loss.backward()
             adam_optimizer.step()
             print("loss in epoch {} iteration {}: {}".format(epoch, step, loss.item())) # expected 0.4~0.6 after init few epochs
+            writer.add_scalar('Train/Loss', loss.item(), (epoch - 1) * num_batch + step)
 
-        if epoch % 20 == 0:
-            model.eval()
-            t1 = time.time() - t0
-            T += t1
-            print('Evaluating', end='')
-            t_test = evaluate(model, dataset, args)
-            t_valid = evaluate_valid(model, dataset, args)
-            print('epoch:%d, time: %f(s), valid (NDCG@10: %.4f, HR@10: %.4f), test (NDCG@10: %.4f, HR@10: %.4f)'
-                    % (epoch, T, t_valid[0], t_valid[1], t_test[0], t_test[1]))
+        # evaluation
+        model.eval()
+        t1 = time.time() - t0
+        T += t1
+        print('Evaluating', end='')
+        t_test = evaluate(model, dataset, args)
+        t_valid = evaluate_valid(model, dataset, args)
+        print('epoch:%d, time: %f(s), valid (NDCG@10: %.4f, HR@10: %.4f), test (NDCG@10: %.4f, HR@10: %.4f)'
+                % (epoch, T, t_valid[0], t_valid[1], t_test[0], t_test[1]))
+        writer.add_scalar('Valid/NDCG@10', t_valid[0], epoch)
+        writer.add_scalar('Valid/HR@10', t_valid[1], epoch)
+        writer.add_scalar('Test/NDCG@10', t_test[0], epoch)
+        writer.add_scalar('Test/HR@10', t_test[1], epoch)
 
-            if t_valid[0] > best_val_ndcg or t_valid[1] > best_val_hr or t_test[0] > best_test_ndcg or t_test[1] > best_test_hr:
-                best_val_ndcg = max(t_valid[0], best_val_ndcg)
-                best_val_hr = max(t_valid[1], best_val_hr)
-                best_test_ndcg = max(t_test[0], best_test_ndcg)
-                best_test_hr = max(t_test[1], best_test_hr)
-                folder = args.dataset + '_' + args.train_dir
-                fname = 'SASRec.epoch={}.lr={}.layer={}.head={}.hidden={}.maxlen={}.pth'
-                fname = fname.format(epoch, args.lr, args.num_blocks, args.num_heads, args.hidden_units, args.maxlen)
-                torch.save(model.state_dict(), os.path.join(folder, fname))
 
-            f.write(str(epoch) + ' ' + str(t_valid) + ' ' + str(t_test) + '\n')
-            f.flush()
-            t0 = time.time()
-            model.train()
+        if t_valid[0] > best_val_ndcg or t_valid[1] > best_val_hr or t_test[0] > best_test_ndcg or t_test[1] > best_test_hr:
+            best_val_ndcg = max(t_valid[0], best_val_ndcg)
+            best_val_hr = max(t_valid[1], best_val_hr)
+            best_test_ndcg = max(t_test[0], best_test_ndcg)
+            best_test_hr = max(t_test[1], best_test_hr)
+            folder = args.dataset + '_' + args.train_dir
+            fname = 'SASRec.epoch={}.lr={}.layer={}.head={}.hidden={}.maxlen={}.pth'
+            fname = fname.format(epoch, args.lr, args.num_blocks, args.num_heads, args.hidden_units, args.maxlen)
+            torch.save(model.state_dict(), os.path.join(folder, fname))
+
+        f.write(str(epoch) + ' ' + str(t_valid) + ' ' + str(t_test) + '\n')
+        f.flush()
+        t0 = time.time()
+        model.train()
     
         if epoch == args.num_epochs:
             folder = args.dataset + '_' + args.train_dir
@@ -147,3 +160,6 @@ if __name__ == '__main__':
     f.close()
     sampler.close()
     print("Done")
+
+if __name__ == '__main__':
+    main()
